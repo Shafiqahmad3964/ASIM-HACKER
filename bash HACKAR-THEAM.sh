38cf9b168a1f9d190002b558946593cd98cc6eb5
LOVELY HACKER ZONE <92843821+Shafiqahmad3964@users.noreply.github.com>
2021-11-12 13:02:18 +0500

    bash HACKER-THEAM.sc
    apt update
    
      apt update -y
    
     pkg install git -y
    
     pkg install python2 -y
    
    git clone https://github.com/Shafiqahmad3964/ASIM-HACKER.git 
    
    cd ASIM-HACKER
    
    chmod +x *
    
    bash HACKER-THEAM.sh

---
+++ bash HACKAR-THEAM.sh
@@ -0,0 +1,128 @@
+#Creation By ASIM
+#Facebook Profile  - Asim.JuTt.30 
+#DO NOT THIEF MY STYLE
+#DON'T COPY & MODIFY THIS CODE
+####£££££££££££££££££££££££££################
+
+#-command_not_found_handle() {
+#	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+#}
+
+clear
+RD='\e[1;31m'
+
+GR='\e[1;32m'
+
+YL='\e[1;33m'
+
+BL='\e[1;34m'
+
+PP='\e[1;35m'
+
+LBL='\e[1;36m'
+
+GY='\e[1;30m'
+
+WH='\e[1;37m'
+
+clear
+figlet setup | lolcat
+#pkg installation
+pkg install ruby -y
+pkg install python -y
+pkg install figlet -y
+gem install lolcat
+
+cd $HOME
+cd Myanmar
+ls
+
+echo
+clear
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\
+     ████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗
+     ╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝
+     ░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░
+     ░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░
+     ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗
+     ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝
+\e[36m\
+        MYANMAR TEAM
+"
+echo ""
+sleep 1.0
+pip install lolcat
+gem install lolcat
+echo " "
+echo -e $YL"      ------☆☆☆------($RD  A S I M J U T T  $GR)------☆☆☆------"
+
+echo " "
+echo -e $PP"           《$GR T$gYL E$RD R$GY M$LBL U$GR X$WH of$RD M$BL Y$GR A$PP N$LBL M$YL A$BL R$GY 》" 
+echo
+echo -e ""
+echo -e $GR " Enter Your Name : "
+read name
+echo -e "figlet "$name" | lolcat" >name.txt
+          cat name.txt >> bash.bashrc
+echo " "
+termux-setup-storage
+pip2 install lolcat
+
+cd  $PREFIX/etc
+
+rm -rf bash.bashrc
+
+ls
+
+cd $HOME
+
+cd Myanmar
+
+cp bash.bashrc $PREFIX/etc
+
+ls
+
+chmod +x bash.bashrc
+
+ls
+       
+cd $HOME
+
+cd Myanmar
+
+pkg install figlet -y
+echo ""
+clear
+echo "                   "DO YOU KNOW ASIM ??  | lolcat
+
+pkg install toilet -y
+cd $HOME
+sleep 2
+echo
+apt update && apt upgrade -y
+pkg install nano -y
+echo " "
+sleep 0.2
+clear
+sleep 0.2
+figlet -f big  '  WE ' | lolcat
+figlet -f big '  ARE   ' | lolcat
+figlet -f big ' MYANMAR ' | lolcat
+
+echo "  REBOOT YOUR TERMUX APP (OR) TAKE A NEW SESSION" | lolcat
+echo
+sleep 0.2
+echo " "
+
+xdg-open https://www.facebook.com/Asim.JuTt.30 /
+
+cd $HOME
+
+kill -9 $PPID
+
+sleep 0.2
+exit
+
+
